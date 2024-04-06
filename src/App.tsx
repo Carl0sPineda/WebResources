f8369c76e4b70cc8f4b0822471320f5d7948e958
Carl0sPineda <carlos7712p@gmail.com>
2024-04-06 10:43:16 -0600

    chore: Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import PostDetail from "./components/PostDetail";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<HomeTwo />} /> */}
+          <Route path="/:id" element={<PostDetail />} />
+          {/* <Route path="/post-2" element={<Post2 />} /> */}
+        </Routes>
+      </Router>
+      {/* <Footer /> */}
+    </>
+  );
 };
 
 export default App;
