b9af8738a08ba6d315be8c5a857cd01e37d97d2f
Carl0sPineda <carlos7712p@gmail.com>
2024-04-06 15:22:33 -0600

    chore: Not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+interface NotFoundProps {
+  title: string;
+  message: string;
+  buttonText: string;
+}
+
+const NotFound = ({ title, message, buttonText }: NotFoundProps) => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-8xl font-bold text-gray-200">{title}</h1>
+      <span className="text-4xl font-medium text-gray-200">{message}</span>
+      <Link to={"/"} className="mt-4 text-xl text-blue-300 hover:underline">
+        {buttonText}
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
